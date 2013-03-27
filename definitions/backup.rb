2136cb43de7afddf69e115e98a420afb74fba638
Daniel Koepke <daniel.koepke@kwarter.com>
2013-03-27 09:23:12 -0700

    Do not hard-code us-east-1 in RAID snapshotting.

--- definitions/backup.rb
+++ definitions/backup.rb
@@ -1,6 +1,12 @@
 define :generate_raid_backups do
   aws_creds = data_bag_item(node[:aws][:databag_name], node[:aws][:databag_entry])
 #  volumes = node[:backups][:mongo_volumes].join(" ")
+  region = node[:ec2][:placement_availability_zone][0...-1] unless !node[:ec2] or !node[:ec2][:placement_availability_zone]
+
+  if !region
+    region = 'us-west-2'
+    Chef::Log.warn("Could not find ec2.placement_availability_zone attribute for node; defaulting region to #{region} for EBS RAID snapshotting.")
+  end
 
   template "/usr/local/bin/raid_snapshot.sh" do
     source "raid_snapshot.sh.erb"
@@ -8,7 +14,8 @@ define :generate_raid_backups do
     group "root"
     mode "0755"
     variables("seckey" => aws_creds["aws_secret_access_key"],
-              "awskey" => aws_creds["aws_access_key_id"])
+              "awskey" => aws_creds["aws_access_key_id"],
+              "region" => region)
   end
 end
 
